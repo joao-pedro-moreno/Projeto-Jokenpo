eccc193bc41b75db6c78dfb656bac1f471605418
Joao Pedro Moreno <jpmoreno1br@gmail.com>
2021-12-03 20:34:18 -0300

    Alteração do contador

--- script.js
+++ script.js
@@ -184,16 +184,14 @@ function comoJogar() {
 function contador(duration, display) {
     gameStopwatch.style.display = 'block'
 
-    var timer = duration, minutes, seconds
+    var timer = duration, seconds
 
     setInterval(function() {
-        minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10)
 
-        minutes = minutes < 10 ? '0' + minutes : minutes
         seconds = seconds < 10 ? '0' + seconds : seconds
 
-        display.textContent = minutes + ':' + seconds
+        display.textContent = '00:' + seconds
 
         if (--timer < 0) {
             timer = duration
